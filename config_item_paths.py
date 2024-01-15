22c3d55f608e2a15d4c383dbce75a2aae8d79a73
serweryn617 <seweryn.r.1999@gmail.com>
2024-01-15 19:55:31 +0100

    Add simple backup script

---
+++ config_item_paths.py
@@ -0,0 +1,30 @@
+config_item_paths = (
+    (
+        '~/.config/qtile',  # target location on PC
+        'qtile'             # location in this repository
+    ),
+    (
+        '~/.config/alacritty',
+        'alacritty'
+    ),
+    (
+        '~/.config/starship.toml',
+        'starship/starship.toml'
+    ),
+    (
+        '~/.config/picom',
+        'picom'
+    ),
+    (
+        '~/.inputrc',
+        'bash/.inputrc'
+    ),
+    (
+        '~/.bash_aliases',
+        'bash/.bash_aliases'
+    ),
+    (
+        '~/.config/libinput-gestures.conf',
+        'libinput-gestures/libinput-gestures.conf'
+    ),
+)
