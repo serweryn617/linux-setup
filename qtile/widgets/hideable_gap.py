b66aaf8cff0693801edb964f3f71a88eea24f2a3
serweryn617 <seweryn.r.1999@gmail.com>
2024-08-20 16:54:54 +0200

    Fix hideable gap groups

--- qtile/widgets/hideable_gap.py
+++ qtile/widgets/hideable_gap.py
@@ -3,7 +3,7 @@ from libqtile.bar import Gap
 class HideableGap(Gap):
     def __init__(self, size: int) -> None:
         super().__init__(size)
-        self._saved_size = size
+        self._initial_size = size
 
     def is_show(self) -> bool:
         return self._size != 0
@@ -11,6 +11,9 @@ class HideableGap(Gap):
     def show(self, is_show: bool = True) -> None:
         if is_show != self.is_show():
             if is_show:
-                self._size = self._saved_size
+                self._size = self._initial_size
             else:
                 self._size = 0
+
+            if self.screen and self.screen.group:
+                self.screen.group.layout_all()
